81612ed11f9e98f0ba1bb4cf4d0a462a09796692
alijawosti <alijawosti@gmail.com>
2024-02-23 21:42:58 -0600

    Copied home format into new login component and added login section with dummy buttons

---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  providers: [],
+  standalone: true,
+  imports: [MatCardModule]
+})
+export class LoginComponent {
+
+}
