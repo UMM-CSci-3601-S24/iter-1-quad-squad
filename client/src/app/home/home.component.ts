f9249ff8e3e261a9d18dff3566cf647c590b0391
alijawosti <alijawosti@gmail.com>
2024-02-23 22:31:34 -0600

    Create an input UI where the person can choose hunter and can enter invite code to be sent to the game (back end not complete)

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home-component',
@@ -8,14 +9,22 @@ import { MatCardModule } from '@angular/material/card';
     styleUrls: ['./home.component.scss'],
     providers: [],
     standalone: true,
-    imports: [MatCardModule]
+    imports: [MatCardModule, CommonModule]
 })
 
 export class HomeComponent {
-  constructor(private router: Router) { } // Inject the Router service
+  constructor(private router: Router) { }
+  showHunterInput: boolean = false;
 
   hostLogin() {
-    // Redirect to the login page
     this.router.navigate(['/login']);
 }
+
+  showHunterForm() {
+    this.showHunterInput = !this.showHunterInput; // Set the flag to the opposite
+  }
+
+  submitHunterForm() {
+    // NEED TO SEND SOMEWHERE/VALIDATE SOMETHING. DO NOT LEAVE BLANK
+  }
 }
