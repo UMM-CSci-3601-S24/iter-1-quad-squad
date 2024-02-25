3671a1ff5a6a6c486029df00db6568e2550e838b
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-02-25 15:17:42 -0600

    Auto-generated methods

--- server/src/main/java/umm3601/hunt/TaskListController.java
+++ server/src/main/java/umm3601/hunt/TaskListController.java
@@ -1,5 +1,25 @@
 package umm3601.hunt;
 
+import com.mongodb.client.MongoDatabase;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import io.javalin.json.JavalinJackson;
+
 public class TaskListController {
+
+  public TaskListController(MongoDatabase db) {
+    //TODO Auto-generated constructor stub
+  }
+
+public void addRoutes(Javalin mockServer) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'addRoutes'");
+}
+
+public void getTasks(Context ctx) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getTasks'");
+}
 
 }
