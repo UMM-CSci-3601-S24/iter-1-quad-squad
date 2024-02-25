19adcab87c2249c5290b444067cb801b04b68b0b
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-02-25 15:25:21 -0600

    implemented addRoutes for get task by id and get all tasks

--- server/src/main/java/umm3601/hunt/TaskListController.java
+++ server/src/main/java/umm3601/hunt/TaskListController.java
@@ -8,18 +8,32 @@ import io.javalin.json.JavalinJackson;
 
 public class TaskListController {
 
+  private static final String API_TASKS = "api/tasks";
+  private static final String API_TASK_BY_ID = "api/tasks/{id}";
+
   public TaskListController(MongoDatabase db) {
-    //TODO Auto-generated constructor stub
+    // TODO Auto-generated constructor stub
   }
 
-public void addRoutes(Javalin mockServer) {
+  public void getTask(Context ctx) {
     // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'addRoutes'");
-}
+    throw new UnsupportedOperationException("Unimplemented method 'getTask'");
+  }
 
-public void getTasks(Context ctx) {
+  public void getTasks(Context ctx) {
     // TODO Auto-generated method stub
     throw new UnsupportedOperationException("Unimplemented method 'getTasks'");
-}
+  }
+
+  /**
+   * @param server
+   * @param TaskListController
+   */
+  public void addRoutes(Javalin server) {
+    // Get the specified Task (by ID)
+    server.get(API_TASK_BY_ID,this::getTask);
+    // Get the list of all Tasks
+    server.get(API_TASKS, this::getTasks);
+  }
 
 }
