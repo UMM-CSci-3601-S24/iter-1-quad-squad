e9863559cba1500c64b708c9ee9c81a8389a8811
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-02-25 13:53:46 -0600

    Created Hunt.java file

---
+++ server/src/main/java/umm3601/hunt/Hunt.java
@@ -0,0 +1,23 @@
+package umm3601.hunt;
+
+public class Hunt {
+
+  public String _id;
+  public String name;
+  public String desc;
+  public String ownerID;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Hunt)) {
+      return false;
+    }
+    Hunt other = (Hunt) obj;
+    return _id.equals(other._id);
+  }
+
+  @Override
+  public int hashCode() {
+    return _id.hashCode();
+  }
+}
