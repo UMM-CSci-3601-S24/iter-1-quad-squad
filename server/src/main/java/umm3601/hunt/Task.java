65ddd42c87e029a10581d267972d4a6e53b65e26
Tristan Kalvoda <kalvo007@morris.umn.edu>
2024-02-25 14:00:40 -0600

    Created Task.java, a and overrode equals and hashcode

---
+++ server/src/main/java/umm3601/hunt/Task.java
@@ -0,0 +1,27 @@
+package umm3601.hunt;
+
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+public class Task {
+  @ObjectId @Id
+  public String _id;
+
+  public String description;
+  public String huntID;
+  public int position;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Task)) {
+      return false;
+    }
+    Task other = (Task) obj;
+    return _id.equals(other._id);
+  }
+
+  @Override
+  public int hashCode() {
+    return _id.hashCode();
+  }
+}
