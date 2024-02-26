a61f9c8edc0ed30844599b8248e543f33d48dc09
TyBeas <beasl110@morris.umn.edu>
2024-02-26 13:00:25 -0600

    Implemented HuntListController.java

---
+++ server/src/main/java/umm3601/hunt/HuntListController.java
@@ -0,0 +1,94 @@
+package umm3601.hunt;
+
+import static com.mongodb.client.model.Filters.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.Document;
+import org.bson.UuidRepresentation;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.mongojack.JacksonMongoCollection;
+
+import com.mongodb.client.MongoDatabase;
+
+import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
+
+public class HuntListController {
+
+  private static final String API_HUNTS = "api/tasks";
+  private static final String API_HUNTS_BY_ID = "api/tasks/{id}";
+
+  static final String NAME_KEY = "name";
+  static final String DECRIPTION_KEY = "description";
+  static final String OWNERID_KEY = "ownerId";
+
+  private final JacksonMongoCollection<Hunt> huntCollection;
+
+  public HuntListController(MongoDatabase database) {
+    huntCollection = JacksonMongoCollection.builder().build(
+      database,
+      "hunts",
+      Hunt.class,
+      UuidRepresentation.STANDARD);
+  }
+
+  public void getHunt(Context ctx) {
+    String id = ctx.pathParam("id");
+    Hunt hunt;
+
+    try {
+      hunt = huntCollection.find(eq("_id", new ObjectId(id))).first();
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestResponse("The requested hunt id wasn't a legal Mongo Object ID.");
+    }
+    if (hunt == null) {
+      throw new NotFoundResponse("The requested hunt was not found");
+    } else {
+      ctx.json(hunt);
+      ctx.status(HttpStatus.OK);
+    }
+  }
+
+  public void getHunts(Context ctx) {
+    Bson combindFilter = constructFilter(ctx);
+
+    ArrayList<Hunt> matchingHunts = huntCollection
+      .find(combindFilter)
+      .into(new ArrayList<>());
+
+    ctx.json(matchingHunts);
+    ctx.status(HttpStatus.OK);
+  }
+
+  /**
+   * @param server
+   * @param HuntListController
+   */
+  public void addRoutes(Javalin server) {
+    server.get(API_HUNTS_BY_ID, this::getHunt);
+    server.get(API_HUNTS, this::getHunts);
+  }
+
+  private Bson constructFilter(Context ctx) {
+    List<Bson> filters = new ArrayList<>();
+    if (ctx.queryParamMap().containsKey(NAME_KEY)) {
+      filters.add(eq(NAME_KEY, ctx.queryParam(NAME_KEY)));
+    }
+    if (ctx.queryParamMap().containsKey(DECRIPTION_KEY)) {
+      filters.add(eq(DECRIPTION_KEY, ctx.queryParam(DECRIPTION_KEY)));
+    }
+    if (ctx.queryParamMap().containsKey(OWNERID_KEY)) {
+      filters.add(eq(OWNERID_KEY, ctx.queryParam(OWNERID_KEY)));
+    }
+
+    Bson combinedFilter = filters.isEmpty() ? new Document() : and(filters);
+
+    return combinedFilter;
+  }
+}
