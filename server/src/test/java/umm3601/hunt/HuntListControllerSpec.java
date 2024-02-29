cd01ce1615ba0ddaa3440a3b7fbf2abe2e10ddd0
TyBeas <beasl110@morris.umn.edu>
2024-02-29 10:25:51 -0600

    Fixing Imports

--- server/src/test/java/umm3601/hunt/HuntListControllerSpec.java
+++ server/src/test/java/umm3601/hunt/HuntListControllerSpec.java
@@ -1,8 +1,10 @@
 package umm3601.hunt;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.util.ArrayList;
