5b16a6e4119d746f69bc97a5fe96ced234c1c287
LinneaMay <Linneamay@gilbertson.net>
2024-03-01 20:47:13 -0600

    made tasks page look nicer

--- client/src/app/hunt/task-element.component.ts
+++ client/src/app/hunt/task-element.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Task } from './task';
 import { MatCard, MatCardTitle, MatCardSubtitle, MatCardContent } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-task-list-element',
   templateUrl: './task-element.component.html',
   styleUrls: ['./task-element.component.scss'],
   standalone: true,
-  imports: [MatCard, MatCardTitle, MatCardSubtitle, MatCardContent]
+  imports: [MatCard, MatCardTitle, MatCardSubtitle, MatCardContent, RouterLink]
 })
 export class TaskElementComponent {
 
