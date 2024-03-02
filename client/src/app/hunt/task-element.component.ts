8ccf4d52dd7f62c188bfec306d968213a8d5b0cb
LinneaMay <Linneamay@gilbertson.net>
2024-03-02 13:02:21 -0600

    fixed failing ng test

--- client/src/app/hunt/task-element.component.ts
+++ client/src/app/hunt/task-element.component.ts
@@ -2,16 +2,17 @@ import { Component, Input } from '@angular/core';
 import { Task } from './task';
 import { MatCard, MatCardTitle, MatCardSubtitle, MatCardContent } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 @Component({
   selector: 'app-task-list-element',
   templateUrl: './task-element.component.html',
   styleUrls: ['./task-element.component.scss'],
   standalone: true,
-  imports: [MatCard, MatCardTitle, MatCardSubtitle, MatCardContent, RouterLink]
+  imports: [MatCard, MatCardTitle, MatCardSubtitle, MatCardContent, RouterLink, RouterTestingModule]
 })
 export class TaskElementComponent {
-
+constructor() {}
   @Input() task: Task;
 }
 
