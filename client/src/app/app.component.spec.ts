151f3bb63647aaf854b09d0793ae7c61d11b28fb
alijawosti <alijawosti@gmail.com>
2024-03-05 13:04:15 -0600

    Works questionably

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -4,15 +4,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
     imports: [
-        BrowserAnimationsModule,
         RouterTestingModule,
         MatToolbarModule,
         MatIconModule,
