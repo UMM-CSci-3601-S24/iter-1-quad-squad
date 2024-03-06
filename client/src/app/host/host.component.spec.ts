45cada912d7d2092ff0cf0749e446ba4b52912af
LinneaMay <Linneamay@gilbertson.net>
2024-03-06 12:18:24 -0600

    fixed host component by migrating changes written by alija

--- client/src/app/host/host.component.spec.ts
+++ client/src/app/host/host.component.spec.ts
@@ -1,25 +1,84 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockTaskService } from 'src/testing/task.service.mock';
+import { MockHuntService } from 'src/testing/hunt.service.mock';
+import { Hunt } from '../hunt/hunt';
+import { TaskService } from '../hunt/task.service';
+import { HuntService } from '../hunt/hunt.service';
+import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
 import { HostComponent } from './host.component';
 
+
+const COMMON_IMPORTS: unknown[] = [
+ FormsModule,
+ MatCardModule,
+ MatFormFieldModule,
+ MatSelectModule,
+ MatOptionModule,
+ MatButtonModule,
+ MatInputModule,
+ MatExpansionModule,
+ MatTooltipModule,
+ MatListModule,
+ MatDividerModule,
+ MatRadioModule,
+ MatIconModule,
+ MatSnackBarModule,
+ BrowserAnimationsModule,
+ RouterTestingModule,
+];
+
+
 describe('HostComponent', () => {
-  let component: HostComponent;
-  let fixture: ComponentFixture<HostComponent>;
+ let hostComponent: HostComponent;
+ let fixture: ComponentFixture<HostComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HostComponent]
-    })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(HostComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+ beforeEach(waitForAsync( () => {
+   TestBed.configureTestingModule({
+     imports: [COMMON_IMPORTS, HostComponent],
+     providers: [{ provide: TaskService, useValue: new MockTaskService() },
+       {provide: HuntService, useValue: new MockHuntService()}]
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+   })
+   .compileComponents();
+ }));
+
+
+ beforeEach(() => {
+   fixture = TestBed.createComponent(HostComponent);
+   hostComponent = fixture.componentInstance;
+   fixture.detectChanges();
+ });
+
+
+ it('contains all the hunts', () => {
+   expect(hostComponent.serverFilteredHunts.length).toBe(3);
+ });
+
+
+
+
 
 
 });
+
+
