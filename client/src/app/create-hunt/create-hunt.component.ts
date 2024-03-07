4daa374843410b9e8ca337b5dca2bded9517f556
LinneaMay <Linneamay@gilbertson.net>
2024-03-07 11:20:00 -0600

    resubmission due to async

--- client/src/app/create-hunt/create-hunt.component.ts
+++ client/src/app/create-hunt/create-hunt.component.ts
@@ -4,7 +4,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatOptionModule } from '@angular/material/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
